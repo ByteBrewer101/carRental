09cd8611f58e1bc67ddb06784e873e460e81994b
singhpalak28 <singhpalak28002@gmail.com>
2025-09-22 22:54:23 +0530

    changes10

--- frontend/src/Pages/DashBoard.jsx
+++ frontend/src/Pages/DashBoard.jsx
@@ -1,9 +1,23 @@
+import { MyCard } from "@/components/MyCard";
 import { NavBar } from "@/components/NavBar";
 
 export function DashBoard(){
     return (
-        <div className="bg-black min-h-screen">
-          <NavBar/>
-        </div>        
-    )
+      <div className="bg-black min-h-screen">
+        <div className="flex flex-col w-full justify-center items-center">
+          <NavBar />
+          <div className="dark w-[80%] grid lg:grid-cols-4 md:grid-cols-3 gap-4">
+            <MyCard />
+            <MyCard />
+            <MyCard />
+            <MyCard />
+            <MyCard />
+            <MyCard />
+            <MyCard />
+            <MyCard />
+            <MyCard />
+          </div>
+        </div>
+      </div>
+    );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Dropdown } from "./Dropdown";
 
 export function NavBar(){
     return (
-      <div className="flex justify-center items-center p-7">
+      <div className="flex justify-center items-center py-7 w-full">
       <div className="bg-white/5 shadow shadow-lg shadow-white/10 p-2 py-3 w-[80%] border-1 border-white/10 rounded-sm px-7 flex justify-between items-center">
           
         <div className="text-white font-bold text-2xl">SiteName</div>
